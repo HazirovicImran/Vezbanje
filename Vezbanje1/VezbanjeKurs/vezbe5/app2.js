5753a2f6768d96527c355f52d74e5003cde95bba
HazirovicImran <ihazirovic11@gmail.com>
2022-12-25 17:16:12 +0100

    Commit

---
+++ Vezbanje1/VezbanjeKurs/vezbe5/app2.js
@@ -0,0 +1,22 @@
+
+const promise = new Promise((resolve, reject) => {
+    resolve();
+});
+
+promise.then(() => {
+    promise.then(() => {
+        console.log("2");
+    })
+    console.log("1");
+});
+
+promise.then(() => {
+    console.log("3");
+})
+
+
+// Prvo se izvrsavaju sve sinhrone radnje unutar svih promisa a onda dolaze na red asinhrone operacije.
+// Redosled:
+// 1
+// 3
+// 2
