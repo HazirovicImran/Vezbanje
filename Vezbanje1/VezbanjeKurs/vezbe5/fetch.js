8a9b13387add4eaf476908fe1829b132ecb0c68f
HazirovicImran <ihazirovic11@gmail.com>
2022-12-25 18:00:34 +0100

    Commit

--- Vezbanje1/VezbanjeKurs/vezbe5/fetch.js
+++ Vezbanje1/VezbanjeKurs/vezbe5/fetch.js
@@ -6,3 +6,12 @@ fetch("https://jsonplaceholder.typicode.com/posts")
         posts.forEach((post) => console.log(post.title))
     })
     .catch((err) => console.log(err))
+
+// Pravljenje asinhrone funkcije:
+const getUsers = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    return users;
+}
+
+getUsers().then((users) => console.log(users));
