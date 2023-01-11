1a0ccb419633933421bad89ed1cc8482fdc085a2
HazirovicImran <ihazirovic11@gmail.com>
2023-01-11 19:52:10 +0100

    Commit

--- Vezbanje1/VezbanjeKurs/vezbe6/classWork.js
+++ Vezbanje1/VezbanjeKurs/vezbe6/classWork.js
@@ -17,19 +17,80 @@
 
 // newCar.fullName();
 
+// Create a new class User with data like name, lName(last name), yearOfBirth
+// Create a method which will calculate how old is user.
+
 class User {
-    constructor(firstName, lastName) {
+    constructor(firstName, lastName, yearOfBirth) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+        this.age = this.calculateAge(yearOfBirth);
     }
 
     fullName() {
         console.log(this.firstName + " " + this.lastName);
     }
+
+    calculateAge() {
+        return 2022 - this.yearOfBirth;
+    }
 }
 
-const newUser = new User("Imran", "Hazirovic");
+
+
+class AdditionalInfo extends User {
+    statesArr = [
+        {
+            state: "USA",
+            cId: "+1",
+        },
+        {
+            state: "Mexico",
+            cId: "+52"
+        },
+        {
+            state: "Germany",
+            cId: "+49"
+        },
+        {
+            state: "Serbia",
+            cId: "+381"
+        },
+        {
+            state: "BiH",
+            cId: "+387"
+        },
+        {
+            state: "Montenegro",
+            cId: "+382"
+        },
+    ];
+
+    // street, city, state, phone number
+    constructor(street, city, state, phoneNumber) {
+        super(newUser.fullName, newUser.lastName, newUser.yearOfBirth, newUser.age)
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.phoneNumber = phoneNumber;
+        this.number = this.numberHandler(this.state, this.cId)
+    }
+
+    numberHandler(state, phoneNumber) {
+        this.findState = this.statesArr.find(s => s.state === state);
+        return phoneNumber.replace(phoneNumber[0], this.findState.cId);
+    }
+}
+
+
+
+const newUser = new AdditionalInfo("Imran", "Hazirovic", 2003, "Kraljevica Marka 17", "Novi Pazar", "0645256656");
 
 console.log(newUser);
 
 newUser.fullName();
+
+const additional = new AdditionalInfo("Imran", "Hazirovic", 2003, "Kraljevica Marka 17", "Novi Pazar", "0645256656");
+
+console.log(additional);
---
+++ Vezbanje1/VezbanjeKurs/vezbe7/class-5.js
@@ -0,0 +1,19 @@
+document.getElementById("someID");
+document.getElementsByClassName("someClass");
+document.getElementsByTagName("tagName");
+document.querySelector("#someID or .className");
+
+document.createElement("p");
+document.removeChild("p");
+document.appendChild("p");
+document.replaceChild("new", "old");
+document.write("text");
+
+document.getElementById("id").onclick = function () {
+    // some in
+};
+
+// Create a to-do app where users can enter their daily plan
+// Users can click the plan and after it's clicked
+// it's marked as DONE.
+
