541def35c2dc362e00472aa4af20946a88f7e57b
HazirovicImran <ihazirovic11@gmail.com>
2023-01-18 21:02:10 +0100

    Commit

--- Vezbanje1/VezbanjeKurs/nitshop/src/App.jsx
+++ Vezbanje1/VezbanjeKurs/nitshop/src/App.jsx
@@ -1,15 +1,26 @@
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import StyledButton from "./components/StyledButton/styledButton";
 import StyledHeader from "./components/StyledHeader/styledHeader";
 
 const App = () => {
+	const [innerText, setInnerText] = useState("");
+	const [clicked, setClicked] = useState(false);
+
+	useEffect(() => {
+		clicked ? setInnerText("Clicked") : setInnerText("Click me!");
+	}, [clicked]);
+
+	function buttonClick() {
+		setClicked(!clicked);
+	}
+
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<StyledHeader innerText="This is header" />
-				<StyledButton innerText="Click" />
+				<StyledButton innerText={innerText} onClickHandler={buttonClick} />
 				<a
 					className="App-link"
 					href="https://reactjs.org"
--- Vezbanje1/VezbanjeKurs/nitshop/src/components/StyledButton/styledButton.jsx
+++ Vezbanje1/VezbanjeKurs/nitshop/src/components/StyledButton/styledButton.jsx
@@ -2,16 +2,8 @@ import React from "react";
 import "./styledButton.css";
 
 const StyledButton = (props) => {
-	var ButtonText = "Click me";
-
-	function buttonHandler() {
-		console.log("Clicked!");
-		ButtonText = "CLICKED!";
-		return ButtonText;
-	}
-
 	return (
-		<button className="styledButton" onClick={() => buttonHandler()}>
+		<button className="styledButton" onClick={props.onClickHandler}>
 			{props.innerText}
 		</button>
 	);
